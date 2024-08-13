6cea4685d20ca9a5ddfc0d6eceb57bbe59aa8508
AndersonG2021 <agbc1@discente.ifpe.edu.br>
2024-08-13 19:03:44 -0300

    Prática 02 - parte 0 - Preparação e Ajustes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,5 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation(libs.material3)
+    implementation(libs.androidx.navigation.compose)
 }
